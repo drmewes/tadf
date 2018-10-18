744a6c4d0d671ed0e3d7960f594d44be681bccb2
Stefanie Andrea Mewes (humboldt <mewes1@ctcp-cluster.cm.cluster>
2018-10-19 09:07:57 +1300

    update calcs

--- ANDBA-OMe+MeEt/s1opt.sh
+++ ANDBA-OMe+MeEt/s1opt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=.data.mewes1.CALC.TADF.ANDBA-OMe+MeEt.s1opt.in
-#SBATCH -t 0-12:00
+#SBATCH --job-name=dCALC.TADF.ANDBA-OMe+MeEt.s1opt.in
+#SBATCH -t 3-0:00
 #SBATCH -n 8
 #SBATCH -N 1
 #SBATCH --mem-per-cpu=2G
@@ -23,17 +23,17 @@ echo "The local scratch directory (located on the compute node) is:"
 echo "$SCRATCH"
 echo ""
 
-module add q-chem/intel/QCHEM_5.1.0_Trunk
-#module load openmpi/intel/64/1.10.4-i8 
+module add q-chem/intel/QCHEM_5.0.1_Trunk
+#module load openmpi/intel/64/1.10.1 
 #module load intel/compiler/64/16.0.3/2016.3.210 
 #module load gcc/5.2.0
 #module load intel/mkl/64/11.3.3/2016.3.210
 #export QC=/home/mewes/SOFTWARE/QCHEM_5
-#export QCAUX=/home/mewes/SOFTWARE/QCHEM_AUX_5.0.2
+#export QCAUX=/home/mewes/SOFTWARE/QCHEM_AUX
 export QCSCRATCH=$SCRATCH
 export QCPLATFORM=LINUX_Ix86
-#export QCTHREADS=8
-#export OMP_NUM_THREADS=8
+export QCTHREADS=8
+export OMP_NUM_THREADS=8
 
 # Go into Scratch
 cd $SCRATCH
