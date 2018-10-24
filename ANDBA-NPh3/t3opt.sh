0f056fd4e381d4c6f40e9e2c9dc167f6fe37ad11
Jan Mewes (humboldt <mewes@ctcp-cluster.cm.cluster>
2018-10-24 15:53:03 +1300

    todays final

--- ANDBA-NPh3/t3opt.sh
+++ ANDBA-NPh3/t3opt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=.data.mewes.CALC.TADF.ANDBA-NPh3.t3opt.in
+#SBATCH --job-name=dCALC.TADF.ANDBA-NPh3.t3opt.in
 #SBATCH -t 3-0:00
 #SBATCH -n 8
 #SBATCH -N 1
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH -o /home/mewes/err/qchem-%j
 #SBATCH --mail-user=janmewes@janmewes.de
-#SBATCH -p bigmem
+#SBATCH -p short
 
 echo "This job was submitted from the computer:"
 echo "$SLURM_SUBMIT_HOST"
@@ -24,10 +24,10 @@ echo "$SCRATCH"
 echo ""
 
 module add q-chem/intel/QCHEM_5.0.1_Trunk
-module load openmpi/intel/64/1.10.1 
-module load intel/compiler/64/16.0.3/2016.3.210 
-module load gcc/5.2.0
-module load intel/mkl/64/11.3.3/2016.3.210
+#module load openmpi/intel/64/1.10.1 
+#module load intel/compiler/64/16.0.3/2016.3.210 
+#module load gcc/5.2.0
+#module load intel/mkl/64/11.3.3/2016.3.210
 #export QC=/home/mewes/SOFTWARE/QCHEM_5
 #export QCAUX=/home/mewes/SOFTWARE/QCHEM_AUX
 export QCSCRATCH=$SCRATCH
