b71d844221fc68ca800f2f2c4d392162868b2bd7
Jan Mewes (humboldt <mewes@ctcp-cluster.cm.cluster>
2018-11-01 11:52:17 +1300

    daily update

---
+++ ANDBA-DiEt-OMe/MOM_OPT/PLOTS_tLE/TRIPLETS@T1_TDA/convert.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+convert es_1_trip_1det.cube.tga es_1_trip_1det.cube.png
+rm es_1_trip_1det.cube.tga
+convert es_1_trip_2att.cube.tga es_1_trip_2att.cube.png
+rm es_1_trip_2att.cube.tga
+convert es_1_trip_3dif.cube.tga es_1_trip_3dif.cube.png
+rm es_1_trip_3dif.cube.tga
+convert es_2_trip_1det.cube.tga es_2_trip_1det.cube.png
+rm es_2_trip_1det.cube.tga
+convert es_2_trip_2att.cube.tga es_2_trip_2att.cube.png
+rm es_2_trip_2att.cube.tga
+convert es_2_trip_3dif.cube.tga es_2_trip_3dif.cube.png
+rm es_2_trip_3dif.cube.tga
+convert es_3_trip_1det.cube.tga es_3_trip_1det.cube.png
+rm es_3_trip_1det.cube.tga
+convert es_3_trip_2att.cube.tga es_3_trip_2att.cube.png
+rm es_3_trip_2att.cube.tga
+convert es_3_trip_3dif.cube.tga es_3_trip_3dif.cube.png
+rm es_3_trip_3dif.cube.tga
