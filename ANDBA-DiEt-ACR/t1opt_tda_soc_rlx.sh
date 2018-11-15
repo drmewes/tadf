c1c4c6c1adf959f76f1167abb0f471354dc2f1c7
Jan Mewes (humboldt <mewes@ctcp-cluster.cm.cluster>
2018-11-15 13:32:27 +1300

    added iDBP-ACN and DABNA-PXZ

---
+++ ANDBA-DiEt-ACR/t1opt_tda_soc_rlx.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#SBATCH --job-name=dCALC.TADF.ANDBA-DiEt-ACR.t1opt_tda_soc_rlx.in
+#SBATCH -t 3-0:00
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH -o /home/mewes/err/qchem-%j
+#SBATCH --mail-user=janmewes@janmewes.de
+#SBATCH -p short
+
+echo "This job was submitted from the computer:"
+echo "$SLURM_SUBMIT_HOST"
+echo "and the directory:"
+echo "$SLURM_SUBMIT_DIR"
+echo ""
+echo "It is running on the compute node:"
+echo "$SLURM_CLUSTER_NAME"
+echo ""
+echo "The local scratch directory (located on the compute node) is:"
+echo "$SCRATCH"
+echo ""
+
+module unload *
+module add q-chem/intel/QCHEM_5.1.0_Trunk
+#module load openmpi/intel/64/1.10.4-i8 
+#module load intel/compiler/64/16.0.3/2016.3.210 
+#module load gcc/5.2.0
+#module load intel/mkl/64/11.3.3/2016.3.210
+#export QC=/home/mewes/SOFTWARE/QCHEM_5
+#export QCAUX=/home/mewes/SOFTWARE/QCHEM_AUX_5.0.2
+export QCSCRATCH=$SCRATCH
+export QCPLATFORM=LINUX_Ix86
+#xport QCTHREADS=8
+#xport OMP_NUM_THREADS=8
+
+# Go into Scratch
+cd $SCRATCH
+cp /data/mewes/CALC/TADF/ANDBA-DiEt-ACR/t1opt_tda_soc_rlx.in /data/mewes/CALC/TADF/ANDBA-DiEt-ACR/*.mol .
+
+# Backup old out file is existing 
+[ -e /data/mewes/CALC/TADF/ANDBA-DiEt-ACR/t1opt_tda_soc_rlx.out ] && cp /data/mewes/CALC/TADF/ANDBA-DiEt-ACR/t1opt_tda_soc_rlx.out /data/mewes/CALC/TADF/ANDBA-DiEt-ACR/t1opt_tda_soc_rlx.out_OLD
+
+# Execute the program
+if [ "" = "yes" ] ; then
+$QC/bin/qchem -save t1opt_tda_soc_rlx.in /data/mewes/CALC/TADF/ANDBA-DiEt-ACR/t1opt_tda_soc_rlx.out t1opt_tda_soc_rlx.out.plots
+else
+$QC/bin/qchem t1opt_tda_soc_rlx.in /data/mewes/CALC/TADF/ANDBA-DiEt-ACR/t1opt_tda_soc_rlx.out
+fi
+
